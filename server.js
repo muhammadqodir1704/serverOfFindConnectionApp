5c4468fb891fb984ab09058c4eb7e77ee92fe6e6
Muhammadqodir <goldenparen06@gmail.com>
2025-04-28 00:19:57 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(
+    'mongodb+srv://proffdev_66:gopa0417@cluster0.frak9kw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+  )
+  .then(() => console.log("MongoDBga muvaffaqiyatli ulandi"))
+  .catch((err) => console.error("MongoDBga ulanishda xatolik:", err));
+
+app.get("/", (req, res) => {
+  res.send("Salom, bu backend server!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server ${PORT}-portda ishlamoqda`);
+});
