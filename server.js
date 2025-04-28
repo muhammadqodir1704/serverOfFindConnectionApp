f31b20f5373bf5180445fc47642ae29b30dac6ab
Muhammadqodir <goldenparen06@gmail.com>
2025-04-28 22:38:15 +0500

    w

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
-
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const Person = require("./models/Person");
 require("dotenv").config();
+
+const Person = require("./models/Person"); 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  optionsSuccessStatus: 200
+}));
 app.use(express.json());
 
 mongoose
@@ -20,6 +26,28 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Salom, bu backend server!");
+});
+
+app.get("/api/people", async (req, res) => {
+  try {
+    const people = await Person.find();
+    res.status(200).json(people);
+  } catch (error) {
+    res.status(500).json({ message: "Ma'lumotlarni olishda xatolik", error });
+  }
+});
+
+
+app.post("/api/people", async (req, res) => {
+  const personData = req.body;
+
+  try {
+    const newPerson = new Person(personData);
+    await newPerson.save();
+    res.status(201).json(newPerson);
+  } catch (error) {
+    res.status(500).json({ message: "Ma'lumotlarni saqlashda xatolik", error });
+  }
 });
 
 app.listen(PORT, () => {
